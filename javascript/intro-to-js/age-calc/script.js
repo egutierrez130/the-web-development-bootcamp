a950a2b79f7f309b6af1c969af94b42c58b9af8e
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-08-26 22:02:58 -0700

    Created Age Calcuator that informs the user how mandy days they have been alive. Utilized JS.

---
+++ javascript/intro-to-js/age-calc/script.js
@@ -0,0 +1,5 @@
+var age = prompt("What is your age?");
+var daysAlive = age * 365.25;
+
+alert(age + " years is roughly " + daysAlive + " days.");
+console.log(age + " years is roughly " + daysAlive + " days.");
