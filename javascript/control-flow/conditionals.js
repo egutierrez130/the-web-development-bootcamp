11dc80638ce6d450dc995a849b21d6ee5cb4c9d3
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-08 12:33:27 -0700

    Learning about if, else if, and else conditionals.

---
+++ javascript/control-flow/conditionals.js
@@ -0,0 +1,12 @@
+// Working with Conditionals(if, else if, else)
+
+// Examples
+if(age < 18){
+    console.log("Sorry, you are not old enough to enter the venue");
+}
+else if(age < 21){
+    console.log("You can enter, but cannot drink");
+}
+else {
+    console.log("Come on in. You can drink.");
+}
