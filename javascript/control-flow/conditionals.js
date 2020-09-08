7af00d98a4c609eb7f7c304c930c4851607d98cf
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-08 13:37:20 -0700

    Completed Conditionals Exercise

--- javascript/control-flow/conditionals.js
+++ javascript/control-flow/conditionals.js
@@ -1,12 +1,37 @@
 // Working with Conditionals(if, else if, else)
 
 // Examples
-if(age < 18){
-    console.log("Sorry, you are not old enough to enter the venue");
+// if(age < 18){
+//     console.log("Sorry, you are not old enough to enter the venue");
+// }
+// else if(age < 21){
+//     console.log("You can enter, but cannot drink");
+// }
+// else {
+//     console.log("Come on in. You can drink.");
+// }
+
+// Exercises
+age = Number(age);//turns string to number value.
+
+var age = Number(prompt("What is your age?"));
+
+//If age is negative
+if(age < 0){
+    console.log("Come back once you're out of the womb.");
 }
-else if(age < 21){
-    console.log("You can enter, but cannot drink");
+
+//If age is 21
+if(age === 21){
+    console.log("Happy 21st birthday!");
 }
-else {
-    console.log("Come on in. You can drink.");
+
+//If age is odd
+if (age % 2 !== 0){
+    console.log("Your age is odd");
+}
+
+//If age is a perfect square
+if(age % Math.sqrt(age) === 0){
+    console.log("Your age is a perfect square.");
 }
