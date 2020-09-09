c3a331c46fe9a3c5f460559120e3bfbbfdd9d1e6
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-08 18:36:36 -0700

    Completed First Lesson on While Loops

---
+++ javascript/control-flow/notes.js
@@ -0,0 +1,10 @@
+//While Loops
+//string we're looping over
+var str = "hello";
+//first character is at index 0
+var count = 0;
+
+while(count < str.length) {
+    console.log(str[count]);
+    count++;
+}
