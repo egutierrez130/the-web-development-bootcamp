bcdd77b46ed509ab78bf018c26c33f5d4505e5c0
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-08 21:45:10 -0700

    Completed While Loops Quiz/Problems

--- javascript/control-flow/notes.js
+++ javascript/control-flow/notes.js
@@ -51,3 +51,40 @@
 
 //infinte loop because num will always be less than 150. *do NOT run this portion of code. 
 
+// While Loops Problem Set
+//Problem 1: Print all numbers between -10 and 19
+var num = -10;
+
+while(num <= 19) {
+    console.log(num);
+    num++;
+}
+
+//Problem 2: Print all even numbers between 10 and 40
+var number = 10;
+
+while(number <= 40) {
+    console.log(number);
+    number+= 2;
+}
+
+//Problem 3: Print all odd numbers between 300 and 333
+
+var numero = 301;
+
+while(numero <= 333) {
+    if(numero % 2 !== 0) {
+        console.log(numero);
+    }
+    numero+=1;
+}
+
+//Problem 4. Print all numbers divisible by 5 AND 3 between 5 and 50
+var numb = 5;
+
+while(numb <= 50) {
+    if(numb % 5 === 0 && numb % 3 === 0) {
+        console.log(numb);
+    }
+    numb++;
+}
