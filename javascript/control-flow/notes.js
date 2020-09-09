4068bfffccf575f0f6a501880cbf240314c54940
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-08 18:54:47 -0700

    Completed 3 While Loop Exercises

--- javascript/control-flow/notes.js
+++ javascript/control-flow/notes.js
@@ -1,10 +1,53 @@
 //While Loops
 //string we're looping over
-var str = "hello";
-//first character is at index 0
-var count = 0;
+// var str = "hello";
+// //first character is at index 0
+// var count = 0;
 
-while(count < str.length) {
-    console.log(str[count]);
-    count++;
-}
+// while(count < str.length) {
+//     console.log(str[count]);
+//     count++;
+// }
+
+//While Loops Exercise 1
+// var num = 1;
+
+// while(num <= 10) {
+//     console.log(num);
+//     num += 2;
+// }
+
+//starts at 1 and increments by 2
+//1
+//3
+//5
+//7
+//9
+
+//While Loops Exercise 2
+// var num = 1
+
+// while(num <= 20) {
+//     if(num % 4 === 0){
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+// prints 
+//4
+//8
+//12
+//16
+//20
+
+//While Loops Exercise 3
+// var num = 100;
+
+// while(num < 150) {
+//     console.log(num + 1);
+//     num--;
+// }
+
+//infinte loop because num will always be less than 150. *do NOT run this portion of code. 
+
