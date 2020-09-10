596e68e80addba818386f6f3056382fb22452050
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-09 18:19:35 -0700

    Completed Intro to For Loops

---
+++ javascript/control-flow/forLoop.js
@@ -0,0 +1,16 @@
+// For Loops
+// for(init; condition; step) {
+//     //run some code
+// }
+
+// Printing numbers from 1-5 with a for loop
+for(var count = 1; count < 6; count++) {
+    console.log(count);
+}
+
+// Printing each character in a string with a for loop
+var str = "hello";
+
+for(var i = 0; i < str.length; i++) {
+    console.log(str[i]);
+}
