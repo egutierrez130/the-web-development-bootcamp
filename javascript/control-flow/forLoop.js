6722d36c5e08d1db08484dd973b4914858c1dac1
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-11 19:36:00 -0700

    Completed forLoops Problem Set

--- javascript/control-flow/forLoop.js
+++ javascript/control-flow/forLoop.js
@@ -13,4 +13,52 @@ var str = "hello";
 
 for(var i = 0; i < str.length; i++) {
     console.log(str[i]);
+} 
+
+// For Loops Exercise
+for(var i = 0; i < 16; i+=8) {
+    console.log(i);
+}
+//prints 0 and 8
+
+var str = "ahceclwlxo";
+
+for(var i = 1; i < str.length; i+=2){
+    console.log(str[i]);
+}
+
+console.log("PRINTING ALL NUMBERS BETYWEEN -10 AND 19");
+
+for(var i = -10; i < 20; i++) {
+    console.log(i);
+}
+
+console.log("PRINTING ALL EVEN NUMBERS BETWEEN 10 AND 40");
+
+// for(var i = 10; i <= 40; i += 2) {
+//     console.log(i);
+// }
+
+// or
+
+for(var i = 10; i <= 40; i += 1){
+    if(i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+console.log("PRINTING ALL ODD NUMBERS BETWEEN 300 AND 333");
+
+for(var i = 300; i <= 333; i++){
+    if(i % 2 !== 0) {
+        console.log(i);
+    }
+}
+
+console.log("PRINTING ALL NUMBERS DIVISIBLE BY 5 AND 3 BETWEEN 5 AND 50");
+
+for(var i = 5; i <= 50; i++){
+    if(i % 5 === 0 && i % 3 === 0){
+        console.log(i);
+    }
 }
