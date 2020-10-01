6e727105af86c025cc32577ca084e282b3a41aa0
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-30 23:00:23 -0700

    Intro to Arrays: Different types of arrays and ways to utilize arrays.

--- javascript/arrays/arrays.js
+++ javascript/arrays/arrays.js
@@ -12,3 +12,22 @@ var friends = ["Charlie", "Liz", "David", "Mattias"];
 
 console.log(friends[0]) //"Charlie"
 friends[1] + <3 + friends[2] //"Liz <3 David"
+
+// We can also udpdate arrays
+var friends = ["Charlie", "Liz", "David", "Mattias"];
+
+friends[0] = "Chuck";
+friends[1] = "Lizzie";
+// We can also add new data to an existing array by:
+friends[4] = "Amelie";
+
+// We can initialize an empty array two ways:
+var frineds = []; 
+var friends = new Array()
+
+// Arrays can hold any type of data
+var random_collection =[49, true, "Hermione", null];
+
+// Arrays have a length property
+var nums = [45, 37, 89, 24];
+nums.lenth //4 
