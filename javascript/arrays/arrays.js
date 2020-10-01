685968d5f857feaca41bb4aadb7bcb6fc80636c8
Edgar Gutierrez <edgargutierrez130@gmail.com>
2020-09-30 22:49:39 -0700

    Introduction to Arrays

---
+++ javascript/arrays/arrays.js
@@ -0,0 +1,14 @@
+// Javascript Basics: Arrays
+// Unit Objectives:
+// 1: Define and add data to arrays
+// 2: Utilize built-in array methods
+// 3: Exercise:Console Todo List
+// We're going to use arrays to do things like keep track of that to do's that you have in your To Do list.
+
+// Perfect use case of an array.
+// Let's us group data in a list.
+// Array are indexed starting at 0. 
+var friends = ["Charlie", "Liz", "David", "Mattias"];
+
+console.log(friends[0]) //"Charlie"
+friends[1] + <3 + friends[2] //"Liz <3 David"
